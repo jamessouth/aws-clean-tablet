cdf351e8f8c062c8f12668e568558d8e5eab7f61
jamessouth <jamesdanielsouth@gmail.com>
2021-01-21 01:00:36 -0600

    lobby

--- src/front/components/Lobby.js
+++ src/front/components/Lobby.js
@@ -4,61 +4,65 @@ import { Auth } from "@aws-amplify/auth";
 const ce = React.createElement;
 export default function Lobby() {
     const [connectedWS, setConnectedWS] = useState(false);
-    // const [error, setError] = useState("");
+    const [token, setToken] = useState("");
     console.log("lobbbbbbbb ", connectedWS);
 
     useEffect(() => {
-        async function getUser() {
+        async function getToken() {
             const user = await Auth.currentUserPoolUser();
             const {
                 signInUserSession: {
                     accessToken: { jwtToken },
                 },
             } = user;
-            console.log("nnnn: ", jwtToken);
+            // console.log("nnnn: ", jwtToken);
+            setToken(jwtToken);
         }
-        getUser();
-
-        const ws = new WebSocket(`${process.env.CT_WS}?auth=gotime`);
-        console.log("pojoihuh", Date.now());
-
-        ws.addEventListener(
-            "open",
-            function (e) {
-                setConnectedWS(true);
-                console.log(e, Date.now());
-            },
-            false
-        );
-
-        ws.addEventListener(
-            "error",
-            function (e) {
-                // setConnectedWS(false);
-                // console.log('eeee: ', e);
-                // setError(e.message);
-                console.log(e, Date.now());
-            },
-            false
-        );
-
-        ws.addEventListener(
-            "close",
-            function (e) {
-                setConnectedWS(false);
-                // setError(false);
-                console.log(e, Date.now());
-            },
-            false
-        );
-
-        return function cleanup() {
-            console.log("cleanup");
-            setConnectedWS(false);
-            // setError("");
-            ws.close(1000);
-        };
+        getToken();
     }, []);
+    
+    useEffect(() => {
+        if (token) {
+            const ws = new WebSocket(`${process.env.CT_WS}?auth=${token}`);
+            console.log("pojoihuh", token[0]);
+
+            ws.addEventListener(
+                "open",
+                function (e) {
+                    setConnectedWS(true);
+                    console.log(e, Date.now());
+                },
+                false
+            );//note: remove listeners????
+
+            ws.addEventListener(
+                "error",
+                function (e) {
+                    // setConnectedWS(false);
+                    // console.log('eeee: ', e);
+                    // setError(e.message);
+                    console.log(e, Date.now());
+                },
+                false
+            );
+
+            ws.addEventListener(
+                "close",
+                function (e) {
+                    // setConnectedWS(false);
+                    // setError(false);
+                    console.log(e, Date.now());
+                },
+                false
+            );
+            return function cleanup() {
+                console.log("cleanup");
+                setConnectedWS(false);
+                // setError("");
+                ws.close(1000);
+            };
+        }
+    }, [token]);
 
     // error
     // ? ce("p", null, "connection error, please try again")
