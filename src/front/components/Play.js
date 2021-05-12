81fd612e89847769f7495f6949abe8603ef7c120
jamessouth <jamesdanielsouth@gmail.com>
2021-05-12 14:52:42 -0500

    countdown to play

--- src/front/components/Play.js
+++ src/front/components/Play.js
@@ -9,11 +9,43 @@ export default function Play({history: {action}, user}) {
     const {
         connectedWS,
         game,
+        leadertoken,
         send,
         wsError
     } = useContext(wsContext);
 
     const [count, setCount] = useState(5);
+    const [stopCount, setStopCount] = useState(false);
+
+    useEffect(() => {
+        let id;
+        if (game.playing) {
+            id = setInterval(() => {
+                setCount((c) => c - 1);
+            }, 1000);
+        }
+
+        if (stopCount) {
+            clearInterval(id);
+        };
+        return () => {
+            clearInterval(id);
+        };
+    }, [game.playing, stopCount]);
+
+    useEffect(() => {
+        console.log("cnt play: ", count);
+        if (count === 0) {
+            setStopCount(true);
+            if (game.leader === leadertoken) {
+                send({
+                    action: "play",
+                    gameno: game.no,
+                    type: "start",
+                });
+            }
+        }
+    }, [count, game.leader, leadertoken]);
 
     console.log("playyyyyy ", game, action, user);
 
@@ -30,6 +62,21 @@ export default function Play({history: {action}, user}) {
                 players: game.players,
             }
         ),
+
+
+
+        game.playing
+        ? ce(
+              "p",
+              {
+                  className:
+                      "absolute text-yellow-200 text-2xl font-bold left-1/2 bottom-1/4 transform -translate-x-2/4",
+              },
+              `${count}`
+          )
+        : null,
+
+
         ce(Prompt, {
             when: true,
             message: "You will be ejected from the game!",
