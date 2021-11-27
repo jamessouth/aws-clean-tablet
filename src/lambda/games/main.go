652786ea76c1f12317c9d4e5c1c45e9ddebb0ce9
jamessouth <jamesdanielsouth@gmail.com>
2021-11-27 00:47:00 -0600

    cont game λ sort

--- src/lambda/games/main.go
+++ src/lambda/games/main.go
@@ -65,7 +65,7 @@ func (gl fromDBListGameList) mapListGames() (res toFEListGameList) {
 		res = append(res, toFEListGame{
 			No:      g.Sk,
 			Ready:   g.Ready,
-			Players: g.Players.getListPlayersSlice().sort(name),
+			Players: g.Players.getListPlayersSlice().sort(nameList),
 		})
 	}
 
@@ -121,8 +121,19 @@ type fromDBLiveGame struct {
 	Sk           string        `dynamodbav:"sk"`
 	Players      livePlayerMap `dynamodbav:"players"`
 	ShowAnswers  bool          `dynamodbav:"showAnswers"`
+	CurrentWord  string        `dynamodbav:"currentWord"`
 	AnswersCount int           `dynamodbav:"answersCount"`
 	SendToFront  bool          `dynamodbav:"sendToFront"`
+}
+
+func (pm livePlayerMap) getLivePlayersSlice() (res livePlayerList) {
+	res = make(livePlayerList, 0)
+
+	for _, v := range pm {
+		res = append(res, v)
+	}
+
+	return
 }
 
 // type gamein struct {
@@ -161,33 +172,33 @@ type removeGamePayload struct {
 	RemoveGame toFEListGame `json:"rmvGame"`
 }
 
-type lessFunc func(p1, p2 *listPlayer) int
+type lessFuncList func(p1, p2 *listPlayer) int
 
-type multiSorter struct {
+type multiSorterList struct {
 	players listPlayerList
-	less    []lessFunc
+	less    []lessFuncList
 }
 
-func (ms *multiSorter) Sort(players listPlayerList) {
+func (ms *multiSorterList) Sort(players listPlayerList) {
 	ms.players = players
 	sort.Sort(ms)
 }
 
-func OrderedBy(less ...lessFunc) *multiSorter {
-	return &multiSorter{
+func OrderedByList(less ...lessFuncList) *multiSorterList {
+	return &multiSorterList{
 		less: less,
 	}
 }
 
-func (ms *multiSorter) Len() int {
+func (ms *multiSorterList) Len() int {
 	return len(ms.players)
 }
 
-func (ms *multiSorter) Swap(i, j int) {
+func (ms *multiSorterList) Swap(i, j int) {
 	ms.players[i], ms.players[j] = ms.players[j], ms.players[i]
 }
 
-func (ms *multiSorter) Less(i, j int) bool {
+func (ms *multiSorterList) Less(i, j int) bool {
 	for _, k := range ms.less {
 		switch k(&ms.players[i], &ms.players[j]) {
 		case 1:
@@ -200,7 +211,66 @@ func (ms *multiSorter) Less(i, j int) bool {
 	return true
 }
 
-var name = func(a, b *listPlayer) int {
+func (p listPlayerList) sort(fs ...lessFuncList) listPlayerList {
+	OrderedByList(fs...).Sort(p)
+
+	return p
+}
+
+var nameList = func(a, b *listPlayer) int {
+	if a.Name > b.Name {
+		return -1
+	}
+
+	return 1
+}
+
+type lessFuncLive func(p1, p2 *livePlayer) int
+
+type multiSorterLive struct {
+	players livePlayerList
+	less    []lessFuncLive
+}
+
+func (ms *multiSorterLive) Sort(players livePlayerList) {
+	ms.players = players
+	sort.Sort(ms)
+}
+
+func OrderedByLive(less ...lessFuncLive) *multiSorterLive {
+	return &multiSorterLive{
+		less: less,
+	}
+}
+
+func (ms *multiSorterLive) Len() int {
+	return len(ms.players)
+}
+
+func (ms *multiSorterLive) Swap(i, j int) {
+	ms.players[i], ms.players[j] = ms.players[j], ms.players[i]
+}
+
+func (ms *multiSorterLive) Less(i, j int) bool {
+	for _, k := range ms.less {
+		switch k(&ms.players[i], &ms.players[j]) {
+		case 1:
+			return true
+		case -1:
+			return false
+		}
+	}
+
+	return true
+}
+
+func (p livePlayerList) sort(fs ...lessFuncLive) livePlayerList {
+	OrderedByLive(fs...).Sort(p)
+
+	return p
+}
+
+var nameLive = func(a, b *livePlayer) int {
 	if a.Name > b.Name {
 		return -1
 	}
@@ -217,12 +287,6 @@ var score = func(a, b *livePlayer) int {
 	}
 
 	return 0
-}
-
-func (p listPlayerList) sort(fs ...lessFunc) listPlayerList {
-	OrderedBy(fs...).Sort(p)
-
-	return p
 }
 
 func FromDynamoDBEventAVMap(m map[string]events.DynamoDBAttributeValue) (res map[string]types.AttributeValue, err error) {
@@ -478,31 +542,6 @@ func handler(ctx context.Context, req events.DynamoDBEvent) (events.APIGatewayPr
 
 			if rec.EventName == dynamodbstreams.OperationTypeInsert {
 
-				// gp := modifyLiveGamePayload{
-				// 	ModLiveGame: toFELiveGame{
-				// 		No:           gameRecord.Sk,
-				// 		ShowAnswers:  gameRecord.ShowAnswers,
-				// 		CurrentWord:  gameRecord.CurrentWord,
-				// 		PreviousWord: gameRecord.PreviousWord,
-				// 		Players:      gameRecord.Players.getPlayersSlice().sort(score, name),
-				// 	},
-				// }
-
-				// payload, err := json.Marshal(gp)
-				// if err != nil {
-				// 	return callErr(err)
-				// }
-
-				// for _, v := range gp.ModLiveGame.Players {
-
-				// 	conn := apigatewaymanagementapi.PostToConnectionInput{ConnectionId: aws.String(v.ConnID), Data: payload}
-
-				// 	_, err = apigwsvc.PostToConnection(ctx, &conn)
-				// 	if err != nil {
-				// 		return callErr(err)
-				// 	}
-
-				// }
 			} else if rec.EventName == dynamodbstreams.OperationTypeModify {
 
 				var gameRecord fromDBLiveGame
@@ -521,7 +560,7 @@ func handler(ctx context.Context, req events.DynamoDBEvent) (events.APIGatewayPr
 							ShowAnswers:  gameRecord.ShowAnswers,
 							CurrentWord:  gameRecord.CurrentWord,
 							PreviousWord: gameRecord.PreviousWord,
-							Players:      gameRecord.Players.getPlayersSlice().sort(score, name),
+							Players:      gameRecord.Players.getLivePlayersSlice().sort(score, name),
 						},
 					}
 
