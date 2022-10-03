b87ba2e790e00b03c6418fad60d5631261f05dda
jamessouth <jamesdanielsouth@gmail.com>
2022-10-03 15:51:38 -0500

    add preToken λ

---
+++ src/lambda/preToken/go.mod
@@ -0,0 +1,28 @@
+module preToken
+
+go 1.19
+
+require (
+	github.com/aws/aws-lambda-go v1.34.1
+	github.com/aws/aws-sdk-go v1.44.109
+	github.com/aws/aws-sdk-go-v2/config v1.17.8
+	github.com/aws/aws-sdk-go-v2/service/dynamodb v1.17.1
+	github.com/google/uuid v1.3.0
+)
+
+require (
+	github.com/aws/aws-sdk-go-v2 v1.16.16 // indirect
+	github.com/aws/aws-sdk-go-v2/credentials v1.12.21 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.12.17 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.23 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.4.17 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.3.24 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.9.9 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/endpoint-discovery v1.7.17 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.9.17 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.11.23 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.13.6 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.16.19 // indirect
+	github.com/aws/smithy-go v1.13.3 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+)
---
+++ src/lambda/preToken/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/google/uuid"
+)
+
+func panicProtectedUID() (id string) {
+	defer func() {
+		if err := recover(); err != nil {
+			id = fmt.Sprintf("uuid panic: %v", err)
+		}
+	}()
+
+	return uuid.NewString()
+}
+
+func handler(ctx context.Context, ev events.CognitoEventUserPoolsPreTokenGen) (events.CognitoEventUserPoolsPreTokenGen, error) {
+
+	// fmt.Printf("%s: %+v\n", "event", ev)
+	// fmt.Println(uid)
+
+	uid := panicProtectedUID()
+
+	if strings.HasPrefix(uid, "uuid panic") {
+		return ev, errors.New(uid)
+	}
+
+	cfg, err := config.LoadDefaultConfig(ctx,
+		config.WithRegion(ev.CognitoEventUserPoolsHeader.Region),
+	)
+	if err != nil {
+		return ev, err
+	}
+
+	var (
+		tableName = os.Getenv("tableName")
+		ddbsvc    = dynamodb.NewFromConfig(cfg)
+	)
+
+	_, err = ddbsvc.PutItem(ctx, &dynamodb.PutItemInput{
+		Item: map[string]types.AttributeValue{
+			"pk": &types.AttributeValueMemberS{Value: "TOKEN#" + ev.Request.UserAttributes["sub"]},
+			"sk": &types.AttributeValueMemberS{Value: uid},
+		},
+		TableName: aws.String(tableName),
+	})
+	if err != nil {
+		return ev, err
+	}
+
+	ev.Response.ClaimsOverrideDetails.ClaimsToAddOrOverride = map[string]string{
+		"q": uid,
+	}
+
+	return ev, nil
+}
+
+func main() {
+	lambda.Start(handler)
+}
