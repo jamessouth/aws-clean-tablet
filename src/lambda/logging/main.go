02963cb689018f029561d29eabf3aec6b69b348b
jamessouth <jamesdanielsouth@gmail.com>
2022-11-14 01:43:19 -0600

    add logging route and λ

---
+++ src/lambda/logging/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func handler(ctx context.Context, req events.APIGatewayWebsocketProxyRequest) (events.APIGatewayProxyResponse, error) {
+
+	fmt.Printf("%s: %s\n", "Front end error: ", req.Body)
+
+	return events.APIGatewayProxyResponse{
+		StatusCode:        http.StatusOK,
+		Headers:           map[string]string{"Content-Type": "application/json"},
+		MultiValueHeaders: map[string][]string{},
+		Body:              "",
+		IsBase64Encoded:   false,
+	}, nil
+}
+
+func main() {
+	lambda.Start(handler)
+}
